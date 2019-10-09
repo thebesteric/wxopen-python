75b1b51ed49185a421662005876db47929a1205d
eric <whatisjava@hotmail.com>
2019-10-09 16:10:10 +0800

    add curl support

--- client/request/base/wxrequest.py
+++ client/request/base/wxrequest.py
@@ -8,6 +8,10 @@
 @build: 2019/10/8 14:47
 @info: 
 """
+from io import StringIO
+
+import pycurl
+
 from cache.cache import MemoryCache
 from client.constants import ACCESS_TOKEN
 
@@ -21,8 +25,27 @@ class WeChatRequest:
 
     def __init__(self):
         self.__cache = cache
+        self.pycurl = pycurl
         self.access_token = self.__cache.get(ACCESS_TOKEN)
 
     def get_cache(self):
-        """获取缓存"""
+        """
+        获取缓存
+        :return: MemoryCache
+        """
         return self.__cache
+
+    def get_curl(self):
+        """
+        获取curl
+        :return: (curl, fp)
+        """
+        curl = self.pycurl.Curl()
+        fp = StringIO()
+        curl.setopt(pycurl.WRITEFUNCTION, fp.write)
+        curl.setopt(pycurl.FOLLOWLOCATION, 1)
+        curl.setopt(pycurl.MAXREDIRS, 5)
+        curl.setopt(pycurl.CONNECTTIMEOUT, 60)
+        curl.setopt(pycurl.TIMEOUT, 300)
+        curl.setopt(pycurl.HEADER, 1)
+        return curl, fp
